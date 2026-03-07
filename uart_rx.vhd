1d118870968df7535c0d4b2d03f86fa7c7ad4baa
lian-gur <liangur1212@gmail.com>
2026-03-07 12:24:29 +0200

    Update uart_rx.vhd

--- uart_rx.vhd
+++ uart_rx.vhd
@@ -7,26 +7,24 @@ entity uart_rx is
         g_CLKS_PER_BIT : integer := 5208     -- 50MHz / 9600 baud
     );
     port (
-        i_clk       : in  std_logic;         -- ???? ??????
-        i_rx_serial : in  std_logic;         -- ??? ???? ?????
-        o_rx_dv     : out std_logic;         -- '1' ???? ?? ??? ????
-        o_rx_byte   : out std_logic_vector(7 downto 0) -- ??? ?????
+        i_clk       : in  std_logic;         
+        i_rx_serial : in  std_logic;         
+        o_rx_dv     : out std_logic;         
+        o_rx_byte   : out std_logic_vector(7 downto 0) 
     );
 end uart_rx;
 
 architecture rtl of uart_rx is
-    -- ????? ?????? ?? ??????
     type t_sm_state is (s_IDLE, s_RX_START_BIT, s_RX_DATA_BITS, 
                          s_RX_STOP_BIT, s_CLEANUP);
     signal r_sm_state : t_sm_state := s_IDLE;
     
     signal r_clk_count : integer range 0 to g_CLKS_PER_BIT-1 := 0;
-    signal r_bit_index : integer range 0 to 7 := 0; -- ???? 8 ?????
+    signal r_bit_index : integer range 0 to 7 := 0; 
     signal r_rx_byte   : std_logic_vector(7 downto 0) := (others => '0');
     signal r_rx_dv     : std_logic := '0';
 begin
 
-    -- ????? ????? ???????
     process(i_clk)
     begin
         if rising_edge(i_clk) then
@@ -36,14 +34,13 @@ begin
                     r_clk_count <= 0;
                     r_bit_index <= 0;
                     
-                    if i_rx_serial = '0' then -- ????? Start Bit (????? ?-0)
+                    if i_rx_serial = '0' then 
                         r_sm_state <= s_RX_START_BIT;
                     else
                         r_sm_state <= s_IDLE;
                     end if;
 
                 when s_RX_START_BIT =>
-                    -- ????? ????? ???? ?????? ??? ????? ??? ?? ???
                     if r_clk_count = (g_CLKS_PER_BIT-1)/2 then
                         if i_rx_serial = '0' then
                             r_clk_count <= 0;
@@ -55,7 +52,6 @@ begin
                         r_clk_count <= r_clk_count + 1;
                     end if;
 
-                -- ??? ????? ??? ?????? (Data, Stop) ???? ???...
                 when others =>
                     r_sm_state <= s_IDLE;
             end case;
@@ -65,4 +61,5 @@ begin
     o_rx_dv   <= r_rx_dv;
     o_rx_byte <= r_rx_byte;
     
+
 end rtl;
